3b64cbb10389ba77455293a235a0710f54bfa913
Yuliya <bezverkhaya@gmail.com>
2019-02-21 20:14:03 +0200

    Update test.ts
    css selectors

--- tests/test.ts
+++ tests/test.ts
@@ -1,7 +1,22 @@
 
-describe('Protractor', function () {
+/*describe('Protractor', function () {
     it('Should be alive', function () {
         browser.url('/')
         console.log('--Test passed!')
+    })
+})*/
+
+describe('WDIO', function() {
+    it('Searching elements', function() {
+        browser.url('/about-us-i-1');
+        let body = $('body');
+
+        console.log(body.getText());
+
+        let paragraphs = $$('p');
+
+        paragraphs.forEach(function(p){
+            console.log(p.getText());
+        })
     })
 })
