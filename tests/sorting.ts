e529cababfee63c2a22eb263a48d8ca8d74da80a
Yuliya <bezverkhaya@gmail.com>
2019-03-05 00:10:46 +0200

    Sorting
    - sort by price DESC
    - sort by name  ASC

--- tests/sorting.ts
+++ tests/sorting.ts
@@ -1,35 +1,45 @@
 import { expect } from "chai";
 
 describe("Items search", function() {
-
   beforeEach("Page refreshing", function() {
     browser.url("/");
   });
 
+  const searchInput = 'form[name="search_form"] input[data-type="search"]';
+  const sortByPriceButton = '#box-search-results a[href*="sort=price"]';
+  const sortByNameButton = '#box-search-results a[href*="sort=name"]';
+  const allDucks = '#box-search-results .product';
+
+  function searchWord(word) {
+    $(searchInput).setValue(word);
+    $(searchInput).addValue("Enter");
+    browser.pause(3000);
+  }
+
   it("correctly arranges items when using 'by price' sorting", function() {
-    const searchInput = $('form[name="search_form"] input[data-type="search"]');
-    searchInput.setValue("duck");
-    searchInput.addValue("Enter");
-    browser.pause(5000);
-
+    searchWord("duck");
     expect(browser.getUrl()).to.contain("query=duck");
-
-    $('#box-search-results a[href*="sort=price"]').click();
+    $(sortByPriceButton).click();
     browser.pause(2000);
-    
+
+    const duckPriceArr = $$(allDucks).map(duck =>parseInt(duck.getAttribute("data-price")));
+    const duckPriceNewArr = duckPriceArr.slice(0); // copy array
+    duckPriceNewArr.sort((a, b) => a - b); // price sorting
+
+    expect(duckPriceArr).to.deep.equal(duckPriceNewArr);
   });
 
   it("correctly arranges items when using 'by name' sorting", function() {
-    const searchInput = $('form[name="search_form"] input[data-type="search"]');
-    searchInput.setValue("duck");
-    searchInput.addValue("Enter"); 
-    browser.pause(5000);
-
+    searchWord("duck");
     expect(browser.getUrl()).to.contain("query=duck");
 
-    $('#box-search-results a[href*="sort=name"]').click();
+    $(sortByNameButton).click();
     browser.pause(2000);
 
-    const sortNameAsc = (a, b) => a.name > b.name ? 1 : -1;
+    const duckNameArr = $$(allDucks).map(duck =>duck.getAttribute("data-name"));
+    const duckNameNewArr = duckNameArr.slice(0); // copy array
+    duckNameNewArr.sort((a, b) => (a > b ? 1 : -1)); // name sorting a-z
+
+    expect(duckNameArr).to.deep.equal(duckNameNewArr);
   });
 });
