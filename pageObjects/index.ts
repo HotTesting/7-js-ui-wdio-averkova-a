b82530ef6ef920e681a59ad195bf7882b1083e37
averkova-a <averkon8@gmail.com@>
2020-10-21 23:09:37 +0300

    remove old homeworks

--- pageObjects/index.ts
+++
@@ -1,3 +0,0 @@
-export { ProductDetails } from "./productDetails";
-export { RegionalSettings} from "./regionalSettings";
-export { ShoppingCart } from './shoppingCart';
--- pageObjects/productDetails.ts
+++
@@ -1,9 +0,0 @@
-import { BasePO } from "./base";
-class ProductDetailsPO extends BasePO {
-  addToCart(): any {
-    $("button.btn-success").click();
-    browser.pause(1000); // yes, i know
-  }
-}
-
-export const ProductDetails = new ProductDetailsPO();
